86d74e91c889a6705fb47d9020599c1030349dfb
Artjom Löbsack <ceigh@pm.me>
2019-04-02 21:11:45 +0500

    feat: Add auto accent color from page bg

--- static/script.js
+++ static/script.js
@@ -262,25 +262,105 @@ function changeBG(url) {
 }
 
 
+// function componentToHex(c) {
+//     var hex = c.toString(16);
+//     return hex.length == 1 ? "0" + hex : hex;
+// }
+//
+// function rgbToHex(r, g, b) {
+//     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+// }
+//
+//
+// function padZero(str, len) {
+//     len = len || 2;
+//     var zeros = new Array(len).join('0');
+//     return (zeros + str).slice(-len);
+// }
+
+
+// function invertColor(hex, bw) {
+//     if (hex.indexOf('#') === 0) {
+//         hex = hex.slice(1);
+//     }
+//     // convert 3-digit hex to 6-digits.
+//     if (hex.length === 3) {
+//         hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+//     }
+//     if (hex.length !== 6) {
+//         throw new Error('Invalid HEX color.');
+//     }
+//     var r = parseInt(hex.slice(0, 2), 16),
+//         g = parseInt(hex.slice(2, 4), 16),
+//         b = parseInt(hex.slice(4, 6), 16);
+//     if (bw) {
+//         // http://stackoverflow.com/a/3943023/112731
+//         return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+//             ? '#000000'
+//             : '#FFFFFF';
+//     }
+//     // invert color components
+//     r = (255 - r).toString(16);
+//     g = (255 - g).toString(16);
+//     b = (255 - b).toString(16);
+//     // pad each with zeros and return
+//     return "#" + padZero(r) + padZero(g) + padZero(b);
+// }
+
+
+function sheet(css) {
+    let style = document.createElement("style");
+	style.appendChild(document.createTextNode(css));
+	document.head.appendChild(style);
+	return style
+}
+
+
 const showSettingsBtn = document.getElementById('settings-icon').children[0];
 const settingsWindow = document.getElementById('settings');
 const saveBGURLBtn = document.getElementById('save-bg-url-btn');
 const clearBGURLBtn = document.getElementById('clear-bg-url-btn');
 const bgURLInput = document.getElementById('bg-url');
+const colorExctractor = document.getElementById('color-extractor');
 let bgURL = getCookie('bg-url');
+let styleElement;
 
 showSettingsBtn.onclick = function () {
     settingsWindow.classList.toggle("closed");
 };
 
+let colorCookie = getCookie('accent');
+if (colorCookie && colorCookie !== '') {
+    styleElement = sheet(`.name,.cost,#bg-url{color:${colorCookie}!important}`);
+} else {
+    styleElement = sheet(`.name,.cost,#bg-url{color:#f39727!important}`);
+}
+
 saveBGURLBtn.onclick = function () {
     setCookie('bg-url', bgURLInput.value);
     changeBG(getCookie('bg-url'));
+
+    // Change Accent color
+    colorExctractor.setAttribute('src', `https://cors-anywhere.herokuapp.com/${bgURLInput.value}`);
+    colorExctractor.addEventListener('load', function () {
+    const vibrant = new Vibrant(this);
+    const swatches = vibrant.swatches();
+    // for (const swatch in swatches)
+    //     if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
+    //         console.log(swatch, swatches[swatch].getHex())
+    //     }
+
+    let dominantRGB = swatches['Vibrant'].getHex();
+    styleElement.innerText = `.name,.cost,#bg-url{color:${dominantRGB}!important}`;
+    setCookie('accent', dominantRGB);
+    });
 };
 
 clearBGURLBtn.onclick = function () {
     bgURLInput.value = '';
     saveBGURLBtn.click();
+    styleElement.innerText = `.name,.cost,#bg-url{color:#f39727!important}`;
+    setCookie('accent', '');
 };
 
 changeBG(bgURL);
