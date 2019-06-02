de40ee08677d542b5e7f793514b6e50306da8186
Artjom Löbsack <ceigh@pm.me>
2019-06-02 16:17:17 +0500

    feat: Add backup stable version on /legacy/

--- auction/views.py
+++ auction/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def legacy_home(request):
+    is_legacy = 'legacy' in request.path
+    return render(request, 'index.html', {'legacy': is_legacy})
