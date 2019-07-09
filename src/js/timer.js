b8c34b49a6be2db2b701dc6a6e024e1e6c122a6e
Artjom Löbsack <ceigh@pm.me>
2019-07-09 12:27:35 +0500

    refactor: Update timer.js to airbnb codestyle

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,10 +1,104 @@
-import ripplet       from 'ripplet.js';
-import cookie        from './cookie';
+// Imports
+import addRipplet from './tools';
+import cookie from './cookie';
 import notifications from './notifications';
-import promotion     from './promotion';
-import winner        from './winner';
+import promotion from './promotion';
+import winner from './winner';
 
 
+// Variables
+const minsCookie = cookie.get('previousMinutes');
+const timerElement = document.getElementById('timer');
+if (minsCookie) timerElement.innerHTML = `${minsCookie}:00:00`;
+
+const timerBtns = Array.from(document.getElementById('timer-btns').children);
+const startBtn = timerBtns[0];
+const pauseBtn = timerBtns[1];
+const stopBtn = timerBtns[2];
+const minusBtn = timerBtns[3];
+const plusBtn = timerBtns[4];
+const plusTwoBtn = timerBtns[5];
+
+
+// Functions
+/**
+ * Convert to '0%d' or %d
+ *
+ * @private
+ * @param {Number} n - number to convert
+ * @return {String} - timer like number string
+ */
+const numToStr = n => (n < 10 ? `0${n}` : n);
+
+/**
+ * Toggle timer color on < 30 sec and 0 min
+ *
+ * @private
+ *
+ * @param {Object} e - link on timer element
+ * @param {Number} m - minutes
+ * @param {Number} s - seconds
+ */
+const toggleDanger = (e, m, s) => {
+  if (!e.classList.contains('danger') && s < 30 && !m) {
+    e.classList.add('danger');
+  } else if (e.classList.contains('danger') && (m || s >= 30)) {
+    e.classList.remove('danger');
+  }
+};
+
+/**
+ * Return winner ransom percent
+ *
+ * @private
+ */
+const returnRansomPercent = () => {
+  const ransom = cookie.get('buyCost');
+
+  if (!ransom) return;
+
+  const costs = Array.from(document.getElementsByClassName('cost'))
+    .map(cost => Number(cost.value));
+  const maxCost = Math.max.apply(null, costs);
+
+  // eslint-disable-next-line consistent-return
+  return Math.round(100 * maxCost / ransom);
+};
+
+/**
+ * Put winner name to document title
+ *
+ * @private
+ */
+const toggleTitle = () => {
+  const { title } = document;
+  const percent = returnRansomPercent();
+  const winnerResult = winner.return();
+
+  let titleStart;
+  if (percent) {
+    titleStart = percent > 99 ? '' : `${percent}%`;
+  } else {
+    titleStart = '';
+  }
+
+  let titleEnd;
+  if (percent) {
+    titleEnd = percent > 99 ? 'сейчас выкупят...' : '';
+  } else {
+    titleEnd = 'лидирует!';
+  }
+
+  let compiled;
+
+  if (winnerResult) {
+    compiled = `${titleStart} ${winner.decorate(winnerResult)} ${titleEnd}`;
+    if (title !== compiled || title === 'Аукцион') document.title = compiled;
+  } else if (title !== 'Аукцион') document.title = 'Аукцион';
+};
+
+
+// Classes
 class Timer {
   /**
    * @constructor
@@ -27,18 +121,20 @@ class Timer {
    *
    * @private
    */
-  debug() {
-    setInterval(() => console.log(
-      'Timer:\n',
-      `   start: ${[this.timeStart.getMinutes(), this.timeStart.getSeconds(),
-                    this.timeStart.getMilliseconds()]}\n\n`,
-      ` started: ${this.started}\n`,
-      `  paused: ${this.paused}\n\n`,
-      `m, s, ms: ${[this.m, this.s, this.ms]}\n`,
-      `    time: ${[this.time.getMinutes(), this.time.getSeconds(),
-                    this.time.getMilliseconds()]}\n`),
-      1000);
-  }
+  // debug() {
+  //   // eslint-disable-next-line no-console
+  //   setInterval(() => console.log(
+  //     'Timer:\n',
+  //     `   start: ${[this.timeStart.getMinutes(), this.timeStart.getSeconds(),
+  //       this.timeStart.getMilliseconds()]}\n\n`,
+  //     ` started: ${this.started}\n`,
+  //     `  paused: ${this.paused}\n\n`,
+  //     `m, s, ms: ${[this.m, this.s, this.ms]}\n`,
+  //     `    time: ${[this.time.getMinutes(), this.time.getSeconds(),
+  //       this.time.getMilliseconds()]}\n`,
+  //   ),
+  //   1000);
+  // }
 
   /**
    * Start timer
@@ -58,15 +154,15 @@ class Timer {
 
       if (!this.paused) {
         this.timeStart = new Date();
-        this.m--;
+        this.m -= 1;
         this.started = true;
         window.started = true;
         // () => ...() to resolve scope problems
         requestAnimationFrame(() => this.updateTimer());
 
         cookie.set('startedTimes', startedTimes + 1);
-        if (0 < startedTimes && !( startedTimes % 15 )) cookie.del('isSupport');
-        if (0 < startedTimes && !isSupport && !( startedTimes % 5 )) {
+        if (startedTimes > 0 && !(startedTimes % 15)) cookie.del('isSupport');
+        if (startedTimes > 0 && !isSupport && !(startedTimes % 5)) {
           setTimeout(promotion.show, 2000);
         }
       } else {
@@ -126,11 +222,14 @@ class Timer {
    *
    */
   minusOne() {
-    let m = --this.m, s = this.s, ms = this.ms;
+    this.m -= 1;
+    let { m } = this;
+    let { s } = this;
+    let { ms } = this;
 
-    this.m = 0 > m ? 0 : m;
+    this.m = m < 0 ? 0 : m;
 
-    if (0 > m) return;
+    if (m < 0) return;
 
     this.time.setMinutes(this.time.getMinutes() - 1);
 
@@ -147,7 +246,8 @@ class Timer {
     m = numToStr(m);
     s = numToStr(s);
     ms = numToStr(ms);
-    ms = String(ms).substr(0, 2);
+    ms = String(ms)
+      .substr(0, 2);
     this.elem.innerHTML = `${m}:${s}:${ms}`;
   }
 
@@ -156,11 +256,14 @@ class Timer {
    *
    */
   plusOne() {
-    let m = ++this.m, s = this.s, ms = this.ms;
+    this.m += 1;
+    let { m } = this;
+    let { s } = this;
+    let { ms } = this;
 
-    this.m = 59 < m ? 59 : m;
+    this.m = m > 59 ? 59 : m;
 
-    if (59 < m) return;
+    if (m > 59) return;
 
     this.time.setMinutes(this.time.getMinutes() + 1);
 
@@ -177,7 +280,8 @@ class Timer {
     m = numToStr(m);
     s = numToStr(s);
     ms = numToStr(ms);
-    ms = String(ms).substr(0, 2);
+    ms = String(ms)
+      .substr(0, 2);
     this.elem.innerHTML = `${m}:${s}:${ms}`;
   }
 
@@ -186,12 +290,14 @@ class Timer {
    *
    */
   plusTwo() {
-    let m = this.m, s = this.s, ms = this.ms;
+    let { m } = this;
+    let { s } = this;
+    let { ms } = this;
 
-    if (59 === m) return;
+    if (m === 59) return;
 
-    this.time.setMinutes(this.time.getMinutes() + ( 58 === m ? 1 : 2 ));
-    m = 59 < m + 2 ? 59 : m + 2;
+    this.time.setMinutes(this.time.getMinutes() + (m === 58 ? 1 : 2));
+    m = m + 2 > 59 ? 59 : m + 2;
     this.m = m;
 
     if (!this.paused) {
@@ -207,7 +313,8 @@ class Timer {
     m = numToStr(m);
     s = numToStr(s);
     ms = numToStr(ms);
-    ms = String(ms).substr(0, 2);
+    ms = String(ms)
+      .substr(0, 2);
     this.elem.innerHTML = `${m}:${s}:${ms}`;
   }
 
@@ -219,9 +326,9 @@ class Timer {
     const current = new Date();
     const delta = new Date(current - this.timeStart);
     const result = new Date(this.time - delta);
-    let m  = result.getMinutes(),
-        s  = result.getSeconds(),
-        ms = result.getMilliseconds();
+    let m = result.getMinutes();
+    let s = result.getSeconds();
+    let ms = result.getMilliseconds();
 
     if (this.paused) return;
 
@@ -233,7 +340,7 @@ class Timer {
     toggleTitle();
 
     // minimal ms can't be 0, and it's totally random
-    if (( !m && !s && 300 > ms ) || this.time < delta) {
+    if ((!m && !s && ms < 300) || this.time < delta) {
       winner.show();
       this.stop();
       return;
@@ -242,31 +349,18 @@ class Timer {
     m = numToStr(m);
     s = numToStr(s);
     ms = numToStr(ms);
-    ms = String(ms).substr(0, 2);
+    ms = String(ms)
+      .substr(0, 2);
     this.elem.innerHTML = `${m}:${s}:${ms}`;
 
     // () => ...() to resolve scope problems
     requestAnimationFrame(() => this.updateTimer());
-  };
+  }
 }
 
 
-const minsCookie = cookie.get('previousMinutes');
-const timerElement = document.getElementById('timer');
-if (minsCookie) timerElement.innerHTML = `${minsCookie}:00:00`;
+// Exec
 const timer = new Timer(timerElement);
-
-const timerBtns = Array.from(document.getElementById('timer-btns').children);
-const startBtn = timerBtns[0];
-const pauseBtn = timerBtns[1];
-const stopBtn = timerBtns[2];
-const minusBtn = timerBtns[3];
-const plusBtn = timerBtns[4];
-const plusTwoBtn = timerBtns[5];
-
-timerBtns.forEach(btn => btn.addEventListener('click', ripplet));
-
-pauseBtn.style.display = 'none';
 
 startBtn.onclick = () => {
   const mins = timerElement.innerHTML.split(':')[0];
@@ -278,7 +372,7 @@ stopBtn.onclick = () => {
   if (!timer.started) {
     timer.reset();
   } else {
-    timer.timeStart = new Date(new Date - timer.time + 300);
+    timer.timeStart = new Date(new Date() - timer.time + 300);
     notifications.clear();
     timer.stop();
   }
@@ -287,62 +381,8 @@ minusBtn.onclick = () => timer.minusOne();
 plusBtn.onclick = () => timer.plusOne();
 plusTwoBtn.onclick = () => timer.plusTwo();
 
+addRipplet(timerBtns);
+
+pauseBtn.style.display = 'none';
+
 // timer.debug();
-
-/**
- * Convert to '0%d' or %d
- *
- * @private
- * @param {Number} n - number to convert
- * @return {String} - timer like number string
- */
-const numToStr = n => 10 > n ? `0${n}` : n;
-
-/**
- * Toggle timer color on < 30 sec and 0 min
- *
- * @private
- *
- * @param {Object} e - link on timer element
- * @param {Number} m - minutes
- * @param {Number} s - seconds
- */
-const toggleDanger = (e, m, s) => {
-  if (!e.classList.contains('danger') && 30 > s && !m) {
-    e.classList.add('danger');
-  } else if (e.classList.contains('danger') && ( m || 30 <= s )) {
-    e.classList.remove('danger');
-  }
-};
-
-/**
- * Put winner name to document title
- *
- * @private
- */
-const toggleTitle = () => {
-  const title = document.title;
-  const percent = returnRansomPercent();
-  const titleStart = percent ? 99 < percent ? '' : `${percent}%` : '';
-  const titleEnd = percent ? 99 < percent ? 'сейчас выкупят...' : '' : 'лидирует!';
-  let winnerResult = winner.return();
-  let compiled;
-
-  if (winnerResult) {
-    compiled = `${titleStart} ${winner.decorate(winnerResult)} ${titleEnd}`;
-    if (title !== compiled || 'Аукцион' === title) document.title = compiled;
-  } else if ('Аукцион' !== title) document.title = 'Аукцион';
-};
-
-const returnRansomPercent = () => {
-  const ransom = cookie.get('buyCost');
-  let costs, maxCost;
-
-  if (!ransom) return;
-
-  costs = Array.from(document.getElementsByClassName('cost'))
-    .map(cost => Number(cost.value));
-  maxCost = Math.max.apply(null, costs);
-
-  return Math.round(100 * maxCost / ransom);
-};
